bb29972bf81c26dc93ea97d8d9e8bad707f8d89c
morez-s <m.sahelgozin@gmail.com>
2024-03-15 17:09:35 +0330

    user profile model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // *********** Relation Functions *********** //
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
